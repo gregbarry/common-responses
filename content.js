00e9ee3c48b72afa01d8b3736768374555eacde8
Greg Barry <greg.barry@sencha.com>
2016-10-13 13:01:17 -0500

    Wrap p tags for forum textarea

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ chrome.runtime.onMessage.addListener(
             // No focused element was found, generally meaning your target is an iframe
             focused = $("iframe");
             focused = focused.contents().find("body")[0];
-
+            template = '<p>' + template.replace(/\n{2}/g, '&nbsp;</p><p>&nbsp;</p><p>').replace(/\n/g, '&nbsp;<br />') + '</p>';
             focused.innerHTML = template;
         } else {
             focused.val(template);
