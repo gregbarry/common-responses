0d76ac14b3d0b361cf38c3a67fa852bee23bef33
Greg Barry <greg.barry@sencha.com>
2016-10-17 18:47:55 -0500

    Add includes, add more error checking, check ids before creating a menu item, update placeholder tags

--- content.js
+++ content.js
@@ -7,20 +7,51 @@ function markupTemplate(template, amount) {
     return template;
 }
 
+function checkForInclude(template) {
+    var re = /.*\$\$.*\$\$.*/;
+
+    return re.test(template);
+}
+
+function getBackgroundData(text, fn) {
+    chrome.runtime.sendMessage({
+        type: 'backgroundData',
+        name: 'bgData',
+        text: text
+    }, function(value) {
+        return fn(value);
+    });
+}
+
+function replaceIncludes(clipTexts, template) {
+    var re = /\$\$\w+\$\$/g,
+        matches = template.match(re), i;
+
+    for (i = 0; i < matches.length; i++) {
+        var match = matches[i],
+            cleanInclude = match.replace('$$', '').replace('$$', ''),
+            index = cleanInclude - 1;
+
+        template = template.replace(match, clipTexts[index]);
+
+        return template;
+    }
+}
+
 function checkForPlaceholder(template) {
-    var re = /.*\{.*}.*/;
+    var re = /.*\[\[.*]].*/;
 
     return re.test(template);
 }
 
 function replacePlaceholders(template) {
     var replacements = {},
-        re = /\{\w+}/g,
+        re = /\[\[\w+]]/g,
         matches = template.match(re), promptValue, i;
 
     for (i = 0; i < matches.length; i++) {
         var match = matches[i],
-            cleanPlaceholder = match.replace('{', '').replace('}', '');
+            cleanPlaceholder = match.replace('[[', '').replace(']]', '');
 
         promptValue = prompt("Please enter " + cleanPlaceholder);
 
@@ -34,36 +65,60 @@ function replacePlaceholders(template) {
     return template;
 }
 
+function checkEntryPoints(template) {
+    var focused     = $(':focus'),
+        textbox     = $("div[role='textbox']:focus"),
+        fb          = $("[data-text='true']"),
+        iframe      = $("iframe"),
+        placeholder = checkForPlaceholder(template);
+
+
+    if (placeholder) {
+        template = replacePlaceholders(template);
+    }
+
+    if (fb.length > 0) {
+        // In the works to post on FaceBook
+        //$(fb[0]).replaceWith("<span data-text='true'>"+template+"</span>");
+    }
+
+    if (focused) {
+        focused.val(template);
+    }
+
+    if (textbox.length > 0) {
+        textbox.html(markupTemplate(template, 'single'));
+    }
+
+    if (iframe.length > 0) {
+        try {
+            iframe = iframe.contents().find("body")[0];
+
+            if (iframe) {
+                iframe.innerHTML = markupTemplate(template, 'double');
+            }
+        } catch (err) {
+
+        }
+    }
+}
+
 chrome.runtime.onMessage.addListener(
     function(request) {
         var template    = request.template,
-            focused     = $(':focus'),
-            textbox     = $("div[role='textbox']:focus"),
-            iframe      = $("iframe"),
-            placeholder = checkForPlaceholder(template);
+            includes    = checkForInclude(template);
 
-        if (placeholder) {
-            template = replacePlaceholders(template);
-        }
+        if (includes) {
+            // We have to do a async callback here because we're getting data from the background
+            // There is new Chrome storage option to look into in the future
+            getBackgroundData('cliptext', function(value){
 
-        if (focused) {
-            focused.val(template);
-        }
-
-        if (textbox.length > 0) {
-            textbox.html(markupTemplate(template, 'single'));
-        }
-
-        if (iframe.length > 0) {
-            try {
-                iframe = iframe.contents().find("body")[0];
-
-                if (iframe) {
-                    iframe.innerHTML = markupTemplate(template, 'double');
-                }
-            } catch (err) {
-
-            }
+                template = replaceIncludes(value, template);
+                checkEntryPoints(template);
+            });
+        } else {
+            checkEntryPoints(template);
         }
     }
 );
+
